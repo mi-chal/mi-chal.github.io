d19e15cb9cd6c547404dcbab35291a52b7e15ecc
mi-chal <31338013+mi-chal@users.noreply.github.com>
2018-12-28 15:17:33 +0100

    Fixed bug in grocery list

--- appetite/service-worker.js
+++ appetite/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65353959e2d4c3a4d61fc61ea7428dd1.js"
+  "/precache-manifest.6068e44cfb8ac9e5345ca6a00cf0ef30.js"
 );
 
 workbox.clientsClaim();
