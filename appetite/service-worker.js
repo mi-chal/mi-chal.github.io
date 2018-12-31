bd1695dde3373ffa6e793d9c4e35114966dde68c
michal <michal@krawiec.me>
2018-12-31 12:02:19 +0100

    Fixing

--- appetite/service-worker.js
+++ appetite/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6068e44cfb8ac9e5345ca6a00cf0ef30.js"
+  "./precache-manifest.a11350c05dc7482e8c30b065f793efa2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
