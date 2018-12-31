e97f965784ebaf445e1debd3ea7f64fb19e87af2
michal <michal@krawiec.me>
2018-12-31 11:26:23 +0100

    Fixing

--- appetite/precache-manifest.6068e44cfb8ac9e5345ca6a00cf0ef30.js
+++ appetite/precache-manifest.6068e44cfb8ac9e5345ca6a00cf0ef30.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = [
   {
     "revision": "87d88a1e4c2f3e7f2fb8054169f3cdc1",
-    "url": "/static/media/timer.87d88a1e.jpeg"
+    "url": "static/media/timer.87d88a1e.jpeg"
   },
   {
     "revision": "6724ffb23a727dc7afab23cbcfed2e1c",
-    "url": "/static/media/meal-plan.6724ffb2.jpeg"
+    "url": "static/media/meal-plan.6724ffb2.jpeg"
   },
   {
     "revision": "090a5c6dc2cb26026fbee346bad67607",
-    "url": "/static/media/grocery-list.090a5c6d.jpeg"
+    "url": "static/media/grocery-list.090a5c6d.jpeg"
   },
   {
     "revision": "9e9faf9bfb78f0bb04646404cad27735",
-    "url": "/static/media/cook-book.9e9faf9b.jpeg"
+    "url": "static/media/cook-book.9e9faf9b.jpeg"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "559913966dd8c0ae1ec3",
-    "url": "/static/js/main.55991396.chunk.js"
+    "url": "static/js/main.55991396.chunk.js"
   },
   {
     "revision": "b31e8bfea76879dbe54c",
-    "url": "/static/js/1.b31e8bfe.chunk.js"
+    "url": "static/js/1.b31e8bfe.chunk.js"
   },
   {
     "revision": "559913966dd8c0ae1ec3",
-    "url": "/static/css/main.bad164ac.chunk.css"
+    "url": "static/css/main.bad164ac.chunk.css"
   },
   {
     "revision": "5af183e606afa73d4ed0faf61a53f285",
-    "url": "/index.html"
+    "url": "index.html"
   }
 ];
