9e32407d627aaaac2ccbd4de033ae79e2afa845e
michal <michal@krawiec.me>
2019-01-17 21:49:11 +0100

    Hotree upload

---
+++ hotree/precache-manifest.bc092ed530e843447627fdf05fb223f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "41af2109b4a946bd2926",
+    "url": "./static/js/main.41af2109.chunk.js"
+  },
+  {
+    "revision": "7469f01b9ccfbaffe0e3",
+    "url": "./static/js/1.7469f01b.chunk.js"
+  },
+  {
+    "revision": "41af2109b4a946bd2926",
+    "url": "./static/css/main.e0d80a28.chunk.css"
+  },
+  {
+    "revision": "6ec956ef73f751b5556140b900faa85b",
+    "url": "./index.html"
+  }
+];
